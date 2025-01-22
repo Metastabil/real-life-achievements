35572a95a75828e94a3d50de724af2bf1d359cd1
Julius Derigs <julius.derigs@mail.de>
2025-01-22 15:42:22 +0100

    Adding the German language file.

--- languages/de.php
+++ languages/de.php
@@ -1,2 +1,142 @@
 <?php
-$lang = [];
+$lang = [
+    'project_title' => 'Real Life Achievements',
+    'nav' => [
+        'achievements' => 'Errungenschaften',
+        'bug_reports' => 'Bug Reports',
+        'categories' => 'Kategorien',
+        'logins' => 'Anmeldungen',
+        'module_functions' => 'Modulfunktionen',
+        'modules' => 'Module',
+        'users' => 'Benutzer',
+        'administration' => 'Administration'
+    ],
+    'actions' => [
+        'save' => 'Speichern',
+        'cancel' => 'Abbrechen',
+        'edit' => 'Bearbeiten',
+        'deleted' => 'Löschen',
+        'show' => 'Anzeigen',
+        'achieved' => 'Errungen',
+        'create' => 'Neu anlegen'
+    ],
+    'messages' => [
+        'saved' => 'Erfolgreich gespeichert',
+        'deleted' => 'Erfolgreich gelöscht',
+        'error' => 'Ein Fehler ist aufgetreten',
+        'not_logged_in' => 'Bitte melde dich an',
+        'no_authorized' => 'Keine Berechtigung'
+    ],
+    'general' => [
+        'yes' => 'Ja',
+        'no' => 'Nein'
+    ],
+    'achievements' => [
+        'titles' => [
+            'index' => 'Errungenschaften',
+            'create' => 'Errungenschaft erstellen',
+            'edit' => 'Errungenschaft bearbeiten',
+            'show' => 'Errungenschaft anzeigen'
+        ],
+        'attributes' => [
+            'name' => 'Name',
+            'description' => 'Beschreibung',
+            'icon' => 'Icon',
+            'user' => 'Benutzer',
+            'category' => 'Kategorien',
+            'active' => 'Aktiv',
+            'created_at' => 'Erstellt am',
+            'updated_at' => 'Aktualisiert am'
+        ]
+    ],
+    'bug_reports' => [
+        'titles' => [
+            'index' => 'Bug Reports',
+            'create' => 'Bug Report erstellen',
+            'edit' => 'Bug Report bearbeiten',
+            'show' => 'Bug Report anzeigen'
+        ],
+        'attributes' => [
+            'name' => 'Name',
+            'description' => 'Beschreibung',
+            'user' => 'Benutzer',
+            'module' => 'Modul',
+            'module_function' => 'Module Funktion',
+            'erledigt' => 'Erledigt',
+            'created_at' => 'Erstellt am',
+            'updated_at' => 'Aktualisiert am'
+        ]
+    ],
+    'categories' => [
+        'titles' => [
+            'index' => 'Kategorien',
+            'create' => 'Kategorie erstellen',
+            'edit' => 'Kategorie bearbeiten',
+            'show' => 'Kategorie anzeigen'
+        ],
+        'attributes' => [
+            'name' => 'Name',
+            'description' => 'Beschreibung',
+            'active' => 'Aktiv',
+            'created_at' => 'Erstellt am',
+            'updated_at' => 'Aktualisiert am'
+        ]
+    ],
+    'logins' => [
+        'titles' => [
+            'index' => 'Logins'
+        ],
+        'attributes' => [
+            'user' => 'Benutzer',
+            'created_at' => 'Erstellt am'
+        ]
+    ],
+    'module_functions' => [
+        'titles' => [
+            'index' => 'Modul Funktionen',
+            'create' => 'Modul Funktion erstellen',
+            'edit' => 'Modul Funktion bearbeiten',
+            'show' => 'Modul Funktion anzeigen'
+        ],
+        'attributes' => [
+            'name' => 'Name',
+            'description' => 'Beschreibung',
+            'active' => 'Aktiv',
+            'module' => 'Modul',
+            'created_at' => 'Erstellt am',
+            'updated_at' => 'Aktualisiert am'
+        ]
+    ],
+    'modules' => [
+        'titles' => [
+            'index' => 'Modul',
+            'create' => 'Modul erstellen',
+            'edit' => 'Modul bearbeiten',
+            'show' => 'Modul anzeigen'
+        ],
+        'attributes' => [
+            'name' => 'Name',
+            'description' => 'Beschreibung',
+            'active' => 'Aktiv',
+            'created_at' => 'Erstellt am',
+            'updated_at' => 'Aktualisiert am'
+        ]
+    ],
+    'users' => [
+        'titles' => [
+            'index' => 'Benutzer',
+            'create' => 'Benutzer erstellen',
+            'edit' => 'Benutzer bearbeiten',
+            'show' => 'Benutzer anzeigen'
+        ],
+        'attributes' => [
+            'username' => 'Benutzername',
+            'email' => 'E-Mail',
+            'password' => 'Passwort',
+            'administrator' => 'Administrator',
+            'active' => 'Aktiv',
+            'created_at' => 'Erstellt am',
+            'updated_at' => 'Aktualisiert am'
+        ]
+    ]
+];
